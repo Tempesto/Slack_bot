18bd378ea253b214caf4ee1f4926f44ef38fa569
Tempesto <igor.it.step@gmail.com>
2019-09-15 00:18:24 +0300

    commit -55

--- sender.py
+++ sender.py
@@ -289,10 +289,10 @@ authed_teams = {}
 def add():
     code_arg = request.args.get('code')
     us_id = request.args.get('state')
+    print('User id =', us_id)
     pre, url = us_id.split('"')
     print('pre =', pre)
     print('url=', url)
-    print('User id =', us_id)
     print('CODE ====', code_arg)
     auth_response = client.oauth_access(
         client_id=SLACK_CLIENT_ID,
